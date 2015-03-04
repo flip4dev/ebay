3a1b35801d2b34857bba88f50ef5ec1ed4a40e05
knoxmic <gh@knoxmic.net>
2015-03-04 14:41:58 +0100

    Fix adding credentials to all requests

--- lib/ebay/request/base.rb
+++ lib/ebay/request/base.rb
@@ -1,5 +1,3 @@
-require 'ebay/types/xml_requester_credentials'
-
 module Ebay
   module Requests
     class Base      
@@ -8,12 +6,6 @@ module Ebay
 
       def call_name
         self.class.to_s.split('::').last.gsub(/Request$/, '')
-      end
-
-      def requester_credentials
-        if auth_token || username || password
-          XMLRequesterCredentials.new(:ebay_auth_token => auth_token, :username => username, :password => password)
-        end
       end
     end
   end
